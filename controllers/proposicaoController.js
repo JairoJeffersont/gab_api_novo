ed909ebc9e187b1bb90c4c9c41631ab17e3f334d
Jairo Santos <jairojeffersont@gmail.com>
2024-12-06 12:05:57 -0300

    v1

--- controllers/proposicaoController.js
+++ controllers/proposicaoController.js
@@ -3,20 +3,90 @@ const { Proposicao, ProposicoesAutores } = require('../models/proposicao');
 const argon2 = require('argon2');
 const { Op } = require('sequelize');
 const axios = require('axios');
+const querystring = require('querystring');
 
-const BATCH_SIZE = 100; // Tamanho do bloco para inserção
+
+require('dotenv').config();
 
 
-// Listar todos os usuários
+const BATCH_SIZE = 100;
+
+
 exports.getProposicoes = async (req, res) => {
     try {
-        const usuarios = await Proposicao.findAll();
+        const autor = req.query.autor || process.env.ID_DEPUTADO;
+        const ano = req.query.ano || new Date().getFullYear();
+        const tipo = req.query.tipo || 'PL';
+        const arquivada = req.query.arquivada || 0;
+        const itens = req.query.itens || 10;
+        const pagina = req.query.pagina || 1;
+        const ordenarPor = req.query.ordenarPor || 'proposicao_id';
+        const ordem = req.query.ordem || 'DESC';
+        const autoria_unica = parseInt(req.query.autoria_unica, 10) ?? 0;
 
-        res.json(usuarios);
+        const limit = parseInt(itens, 10);
+        const offset = (pagina - 1) * limit;
+
+        const whereAutoriaUnica = {};
+
+        if (autoria_unica === 1) {
+            whereAutoriaUnica.proposicao_assinatura = 1;
+            whereAutoriaUnica.proposicao_proponente = 1;
+        }
+
+        const proposicoes = await Proposicao.findAndCountAll({
+            where: {
+                proposicao_ano: ano,
+                proposicao_sigla: tipo,
+                proposicao_arquivada: arquivada,
+            },
+            order: [[ordenarPor, ordem]],
+            limit: limit,
+            offset: offset,
+            include: [
+                {
+                    model: ProposicoesAutores,
+                    as: 'autores',
+                    where: {
+                        proposicao_id_autor: autor,
+                        ...whereAutoriaUnica,
+                    },
+                },
+            ],
+        });
+
+        const totalProposicoes = proposicoes.count
+        const totalPaginas = Math.ceil(totalProposicoes / limit);
+
+        const baseUrl = req.protocol + '://' + req.get('host') + req.baseUrl + '/listar';
+
+        const links = {
+            primeira: `${baseUrl}?${querystring.stringify({ autor, ano, tipo, arquivada, itens, pagina: 1, ordenarPor, ordem, autoria_unica })}`,
+            atual: `${baseUrl}?${querystring.stringify({ autor, ano, tipo, arquivada, itens, pagina, ordenarPor, ordem, autoria_unica })}`,
+            ultima: `${baseUrl}?${querystring.stringify({ autor, ano, tipo, arquivada, itens, pagina: totalPaginas, ordenarPor, ordem, autoria_unica })}`,
+        };
+
+
+        if (proposicoes.count === 0) {
+            return res.status(200).json({ status: 200, message: 'Nenhuma proposição encontrada' });
+        }
+
+        const proposicoesSemAutores = proposicoes.rows.map(proposicao => {
+            const { autores, ...proposicaoSemAutores } = proposicao.dataValues;
+            return proposicaoSemAutores;
+        });
+
+        return res.status(200).json({
+            status: 200,
+            message: proposicoes.count + ' proposições(s) encontrada(s)',
+            dados: proposicoesSemAutores,
+            links: links
+        });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
+
 
 
 exports.atualizarProposicoes = async (req, res) => {
@@ -64,7 +134,7 @@ exports.atualizarAutoresProposicoes = async (req, res) => {
 
         const proposicoesExistentes = await Proposicao.findAll({
             where: { proposicao_ano: ano },
-            attributes: ['proposicao_id'], 
+            attributes: ['proposicao_id'],
         });
 
         const proposicoesIds = new Set(proposicoesExistentes.map(p => p.proposicao_id));
@@ -80,7 +150,7 @@ exports.atualizarAutoresProposicoes = async (req, res) => {
                 proposicao_proponente: item.proponente,
                 proposicao_ano: ano
             }))
-            .filter(autor => proposicoesIds.has(autor.proposicao_id)); // Filtrando apenas autores com proposições válidas
+            .filter(autor => proposicoesIds.has(autor.proposicao_id));
 
         await ProposicoesAutores.destroy({ where: { proposicao_ano: ano } });
 
